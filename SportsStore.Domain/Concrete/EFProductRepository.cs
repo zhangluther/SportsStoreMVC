f59d5cbfcfa02625c82c7c62cd073c2df7d998f3
Zhang Wei <zhangluther@yahoo.com>
2017-07-24 23:04:03 +0800

    0.1.9

--- SportsStore.Domain/Concrete/EFProductRepository.cs
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -36,5 +36,16 @@ namespace SportsStore.Domain.Concrete
             }
             context.SaveChanges();
         }
+
+        public Product DeleteProduct(int productID)
+        {
+            Product dbEntry = context.Products.Find(productID);
+            if (dbEntry != null)
+            {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
+        }
     }
 }
